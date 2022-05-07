f823c95e39b0b19a0e90a9ee7b0bf2450851bb9a
Krishtian Barrera <kd.barrera@uniandes.edu.co>
2022-05-07 11:38:25 -0500

    Kraken KBarrera

---
+++ Kraken/Kraken KBarrera/features/web/step_definitions/step.js
@@ -0,0 +1,74 @@
+const { Given, When, Then } = require('@cucumber/cucumber');
+When ('I enter email {string}', async function (email){
+    let element = await this.driver.$('#ember7');
+    return await element.setValue(email);
+});
+
+When ('I enter password {string}', async function(password){
+    let element = await this.driver.$('#ember9');
+    return await element.setValue(password);
+});
+
+When('I click next', async function() {
+    let element = await this.driver.$('#ember11');
+    return await element.click();
+})
+Then('I click on new post', async function () {
+    let element = await this.driver.$('#ember27');
+    return await element.click();
+});
+Then('I click on Post Title', async function () {
+    let element = await this.driver.$('#ember80');
+    return await element.click();
+});
+Then('I click on Post boddy', async function () {
+    let element = await this.driver.$('#ember81');
+    return await element.click();
+});
+Then('I click on publish', async function () {
+    let element = await this.driver.$('#ember86');
+    return await element.click();
+});
+Then('I click on publish2', async function () {
+    let element = await this.driver.$('#ember96');
+    return await element.click();
+});
+Then('I click on publish3', async function () {
+    let element = await this.driver.$('#ember98');
+    return await element.click();
+});
+Then('I click on post', async function () {
+    let element = await this.driver.$('#ember77');
+    return await element.click();
+});
+Then('I erase post title', async function () {
+    let element = await this.driver.$('#ember80');
+    return await element.setValue('')
+});
+
+Then('I click tags', async function () {
+    let element = await this.driver.$('#ember29');
+    return await element.click()
+});
+Then('I click on new Tag', async function () {
+    let element = await this.driver.$('#ember79');
+    return await element.click();
+});
+Then('I click on save', async function () {
+    let element = await this.driver.$('#ember87');
+    return await element.click();
+});
+Then('I click on tagname', async function () {
+    let element = await this.driver.$('#tag-name');
+    return await element.click();
+});
+
+Then('I click on leave', async function () {
+    let element = await this.driver.$('//button/span[text()="Leave"]');
+    return await element.click();
+});
+
+Then('I click on tagslug', async function () {
+    let element = await this.driver.$('#tag-slug');
+    return await element.click();
+});
